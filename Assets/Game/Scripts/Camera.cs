10e17dc93eef35af6f8f05d6b6b98d07440f2fe0
anatevs <105053045+anatevs@users.noreply.github.com>
2024-02-20 20:20:37 +0300

    camera mech preparation

--- Assets/Game/Scripts/Camera.cs
+++ Assets/Game/Scripts/Camera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Camera : MonoBehaviour
 {
@@ -17,4 +18,22 @@ public class Camera : MonoBehaviour
     }
 
     // follow the player mechanic
+    public class CameraFollowMechanic
+    {
+        private readonly Transform _followerTransform;
+        private readonly Transform _followingTransform;
+        private readonly Vector3 _offset;
+
+        public CameraFollowMechanic(Transform followerTransform, Transform followingTransform, Vector3 offset)
+        {
+            _followerTransform = followerTransform;
+            _followingTransform = followingTransform;
+            _offset = offset;
+        }
+
+        public void Update()
+        {
+            _followerTransform.position = _followingTransform.position + _offset;
+        }
+    }
 }
