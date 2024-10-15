3eb6e650e9e959de361e6ed1c2bc8e860c39991d
anatevs <105053045+anatevs@users.noreply.github.com>
2024-10-15 21:30:19 +0300

    import prefabs, new blackboard

---
+++ Assets/Plugins/AI/Blackboards/Scripts/BlackboardAPI.cs
@@ -0,0 +1,14 @@
+/**
+* Code generation. Don't modify! 
+ */
+using System.Runtime.CompilerServices;
+using Atomic.AI;
+namespace Atomic.AI
+{
+    public static class BlackboardAPI
+    {
+
+
+        ///Extensions
+    }
+}
